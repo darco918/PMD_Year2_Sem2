ee243e15f463d699ff6938c30ae12e338fa71633
Darco Murja <darcomurja@gmail.com>
2021-01-19 01:28:58 +0000

    Start screen, LogIn Act, changed the design of the navigation bar and icons and created the 3 fragments: Profile, Home, Calendar

---
+++ app/src/main/java/com/example/fitnessappiadehackaton/LogInActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fitnessappiadehackaton
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_log_in.*
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("gamertag",username.text )
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/fitnessappiadehackaton/MainActivity.kt
+++ app/src/main/java/com/example/fitnessappiadehackaton/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.fitnessappiadehackaton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_bar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(
+                    lastIndex: Int,
+                    lastTab: AnimatedBottomBar.Tab?,
+                    newIndex: Int,
+                    newTab: AnimatedBottomBar.Tab
+            ) {
+                Log.d("bottom_bar", "Selected index: $newIndex, title: ${newTab.title}")
+            }
+
+            // An optional method that will be fired whenever an already selected tab has been selected again.
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                Log.d("bottom_bar", "Reselected index: $index, title: ${tab.title}")
+            }
+        })
     }
 }
