bcbb7128f499a1a78f65a682fd7b36b0997d4cb4
Darco Murja <darcomurja@gmail.com>
2021-01-25 22:20:19 +0000

    Counter works

--- app/src/main/java/com/example/fitnessappiadehackaton/Workout1Activity.kt
+++ app/src/main/java/com/example/fitnessappiadehackaton/Workout1Activity.kt
@@ -1,10 +1,10 @@
 package com.example.fitnessappiadehackaton
 
+import android.content.Context
+import android.graphics.Color
 import android.graphics.Color.rgb
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.SystemClock
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -14,53 +14,43 @@ import java.lang.reflect.Field
 
 class Workout1Activity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
+    private lateinit var timer: CountDownTimer
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout1)
-
-        startSets()
+        val countDownText = findViewById<TextView>(R.id.countdown)
+        startSets(countDownText)
         setUpSpinners()
-        view_timer.isCountDown = false
-
-        view_timer.start()
-
-
-
-        view_timer.setOnChronometerTickListener {
-            if(view_timer.text == "00:05")
-                set1.setBackgroundResource(R.drawable.round_button)
-
-          //  progressBar.progress =
-
-        }
+        startCounter(180,countDownText)
     }
 
-    private fun startSets() {
+    private fun startSets(countDownText:TextView) {
         val squatArray = arrayOf(set1,set2,set3,set4,set5,set6)
         val benchArray = arrayOf(bench1,bench2,bench3,bench4,bench5,bench6)
         val deadliftArray = arrayOf(dead1,dead2,dead3,dead4,dead5,dead6)
 
-
         for ( i in squatArray){
             i.setOnClickListener {
-                setClick(i)
+                setClick(i,countDownText)
             }
         }
 
         for ( i in benchArray){
             i.setOnClickListener {
-                setClick(i)
+                setClick(i,countDownText)
             }
         }
 
         for ( i in deadliftArray){
             i.setOnClickListener {
-                setClick(i)
+                setClick(i,countDownText)
             }
         }
     }
 
-    private fun setClick(set: Button){
+    private fun setClick(set: Button,countDownText:TextView){
         set.setTextColor(rgb(255,255,255))
         var i = 0
         if(set.text.toString() == "0"){
@@ -69,19 +59,19 @@ class Workout1Activity : AppCompatActivity() {
         else{
             i = set.text.toString().toInt()
         }
-        activateSet(set,i-1)
+        activateSet(set,i-1, countDownText)
     }
 
-    private fun activateSet(set:Button, i : Int){
+    private fun activateSet(set:Button, i : Int, countDownText:TextView){
         set.setBackgroundResource(R.drawable.round_button)
         if(i == 0) {set.text = "0"
             set.setBackgroundResource(R.drawable.round_inactive_button)
             set.setTextColor(rgb(255,0,0))
-
         }
         else         set.text = i.toString()
-
         if(i == 6)   set.setTextColor(rgb(0,255,0))
+        timer.cancel()
+        startCounter(180,countDownText)
     }
 
     private fun setUpSpinners(){
@@ -135,4 +125,35 @@ class Workout1Activity : AppCompatActivity() {
 
     }
 
+    private fun  vibrate(){
+        val v = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(1000,
+                VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+        else {
+            v.vibrate(1000)
+        }
+    }
+
+    private fun startCounter(mCounter:Int, countDownText:TextView){
+       var counter = mCounter
+       timer= object : CountDownTimer((counter*1000).toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                countDownText.text = counter.toString()
+                progressBar.progress =(100- (counter * 5)/9)
+                counter--
+                if((counter == 90) or (counter == 25)){
+                    countDownText.setTextColor(Color.parseColor("#FF0000"))
+                    vibrate()
+                }
+            }
+            override fun onFinish() {
+                counter = 180
+                countDownText.setTextColor(Color.parseColor("#ffffff"))
+                vibrate()
+            }
+        }.start()
+    }
 }
